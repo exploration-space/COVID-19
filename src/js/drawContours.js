4149302a9e54871831216a9710cc7e67c4d30945
Dario Rodighiero <rodighie@Paul-Klee.local>
2020-04-09 14:18:53 -0400

    Contour bug fix

--- src/js/drawContours.js
+++ src/js/drawContours.js
@@ -18,7 +18,7 @@ const _computeDensityData = () => {
         // .weight(d => d.docs)
         .weight(d => sumValues(d.tokens))
         .size([width, height])
-        .cellSize(10)
+        .cellSize(5)
         // If cellSize is specified, sets the size of individual cells in the underlying bin grid to the specified positive integer and returns the estimator. If cellSize is not specified, returns the current cell size, which defaults to 4. The cell size is rounded down to the nearest power of two. Smaller cells produce more detailed contour polygons, but are more expensive to compute.
         .bandwidth(70)
         // If bandwidth is specified, sets the bandwidth (the standard deviation) of the Gaussian kernel and returns the estimate. If bandwidth is not specified, returns the current bandwidth, which defaults to 20.4939…. The specified bandwidth is currently rounded to the nearest supported value by this implementation, and must be nonnegative.
@@ -29,7 +29,8 @@ const _computeDensityData = () => {
     s.densityData.forEach(d => d.coordinates = d.coordinates
         .map(d => d.map(d => d.map(
             d => {
-                return [(Math.floor(d[0] + x)), Math.floor((d[1] + y))]}
+                // return [(Math.floor(d[0] + x)), Math.floor((d[1] + y))]}
+                return [d[0] + x, d[1] + y]}
         )))
     )
 }
