ac2e0de9ec1200f7a78162c2df0198748f27e442
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2020-05-03 21:42:11 -0400

    Zoom activated

--- src/init/pixi.js
+++ src/init/pixi.js
@@ -14,8 +14,30 @@ export default () => {
         autoResize: true,
     })
 
-    document.body.prepend(app.renderer.view)
-    
+    document.body.prepend(app.view)
+
     s.pixi = app.stage
+
+    // create viewport
+    const viewport = new Viewport({
+        screenWidth: window.innerWidth,
+        screenHeight: window.innerHeight,
+        worldWidth: 1000,
+        worldHeight: 1000,
+
+        interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+    })
+
+    // add the viewport to the stage
+    app.stage.addChild(viewport)
+
+    s.pixi = viewport
+
+    // activate plugins
+    viewport
+        .drag()
+        .pinch()
+        .wheel()
+        .decelerate()
 
 }
