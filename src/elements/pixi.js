70711e008d76344983d1e98029def7eca1f6e8e6
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2020-05-10 16:59:57 -0400

    Min and max zoom

--- src/elements/pixi.js
+++ src/elements/pixi.js
@@ -21,10 +21,11 @@ export default () => {
     const viewport = new Viewport({
         screenWidth: window.innerWidth,
         screenHeight: window.innerHeight,
-        worldWidth: 1000,
-        worldHeight: 1000,
+        // worldWidth: 100,
+        // worldHeight: 100,
 
-        interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+        interaction: app.renderer.plugins.interaction
+        // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
     })
 
     // add the viewport to the stage
@@ -38,6 +39,9 @@ export default () => {
         .pinch()
         .wheel()
         .decelerate()
+        .clampZoom({ minScale: .5, maxScale:3 })
+        // .snapZoom({width: 1000})
+
 
     // Prevent pinch gesture in Chrome
 
