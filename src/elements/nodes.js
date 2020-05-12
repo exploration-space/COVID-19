b788562cf6494b91e8fbe06b67ac0af8374789d8
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2020-05-11 21:58:01 -0400

    Label

--- src/elements/nodes.js
+++ src/elements/nodes.js
@@ -1,6 +1,15 @@
 import { mouseover, mouseout } from '../interface/mouseover'
 import * as PIXI from 'pixi.js'
 
+const splitInTwo = string => {
+    const middle = Math.round(string.length / 2)
+    for (let i = middle, j = middle; i < string.length || j >= 0; i++, j--) {
+        if (string[i] === ' ') return [string.substring(0, i), string.substring(i + 1)]
+        if (string[j] === ' ') return [string.substring(0, j), string.substring(j + 1)]
+    }
+    return [string, '']
+}
+
 let stage
 
 export function initNodes() {
@@ -9,27 +18,37 @@ export function initNodes() {
     stage = s.pixi.addChild(nodes)
 
     const nodeStyle = new PIXI.TextStyle({
-        fontFamily: "Arial",
-        fontSize: 6,
-        fill: "white",
+        fontFamily: 'Arial',
+        fontSize: 5,
+        fill: 'white',
+        align: 'center',
     })
 
     s.nodes.forEach(node => {
+
+        // Circle
+
         node.gpx = new PIXI.Graphics()
         node.gpx.beginFill(0xFFFFFF)
         node.gpx.drawCircle(0, 0, 1)
         node.gpx.endFill()
         nodes.addChild(node.gpx)
-        // node.txt = new PIXI.Text(node.name, nodeStyle)
-        // nodes.addChild(node.txt)
+
+        // Label
+
+        const [nA, nB] = splitInTwo(node.name)
+        node.txt = new PIXI.Text(`${nA}\n${nB}`, nodeStyle)
+        node.txt.scale.x = .5
+        node.txt.scale.y = .5
+        nodes.addChild(node.txt)
+
+        // Interaction
+
         node.gpx.interactive = true
         node.gpx.hitArea = new PIXI.Circle(0, 0, 20)
-        node.gpx.mouseover = (mouseData) => {
-            mouseover(node)
-        }
-        node.gpx.mouseout = (mouseData) => {
-            mouseout()
-        }
+        node.gpx.mouseover = mouseData => mouseover(node)
+        node.gpx.mouseout = mouseData => mouseout()
+
     })
 
 }
@@ -42,7 +61,7 @@ export function drawNodes() {
     s.nodes.forEach(node => {
         const { x, y, gpx, txt } = node
         gpx.position = new PIXI.Point(x, y)
-        // txt.position.set(x, y)
+        txt.position.set(x - txt.width / 2, y + 3)
     })
 
 }
